125eb6209c1195c5e041ba8c59d8cb92f01fb6c1
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-08-21 12:05:53 -0300

    Implement optimistic UI

--- react/OrderItems.tsx
+++ react/OrderItems.tsx
@@ -1,13 +1,22 @@
-import React, { createContext, ReactNode, useContext } from 'react'
+import { adjust } from 'ramda'
+import React, {
+  createContext,
+  FunctionComponent,
+  ReactNode,
+  useContext,
+  useState,
+} from 'react'
+import { branch, renderComponent } from 'recompose'
 import { compose, graphql } from 'react-apollo'
 import { useOrderManager } from 'vtex.order-manager/OrderManager'
 
-import * as ItemList from './graphql/itemList.graphql'
-import * as UpdateItem from './graphql/updateItem.graphql'
+import ItemList from './graphql/itemList.graphql'
+import UpdateItem from './graphql/updateItem.graphql'
 
 interface Context {
   itemList: Item[]
-  updateItem: (index: number, quantity: number) => PromiseLike<void>
+  updateItem: (index: number, quantity: number) => void
+  loading: boolean
 }
 
 interface OrderItemsProviderProps {
@@ -18,17 +27,48 @@ interface OrderItemsProviderProps {
 
 const OrderItemsContext = createContext<Context | undefined>(undefined)
 
-const OrderItemsProvider = compose(
-  graphql(ItemList.default, { name: 'ItemListQuery', options: { ssr: false } }),
-  graphql(UpdateItem.default, { name: 'UpdateItem' })
+const EmptyState: FunctionComponent = ({ children }: any) => {
+  const updateItem = async (_: number, __: number) => {}
+  return (
+    <OrderItemsContext.Provider
+      value={{ itemList: [], updateItem, loading: true }}
+    >
+      {children}
+    </OrderItemsContext.Provider>
+  )
+}
+
+let lastUpdateToken = { outdated: false }
+
+export const OrderItemsProvider = compose(
+  graphql(ItemList, { name: 'ItemListQuery', options: { ssr: false } }),
+  graphql(UpdateItem, { name: 'UpdateItem' }),
+  branch(
+    ({ ItemListQuery }: any) => !!ItemListQuery.loading,
+    renderComponent(EmptyState)
+  )
 )(({ children, ItemListQuery, UpdateItem }: OrderItemsProviderProps) => {
   const { enqueue } = useOrderManager()
 
-  const itemList = ItemListQuery.loading ? [] : ItemListQuery.cart.items
+  const [itemList, setItemList] = useState(ItemListQuery.cart.items)
 
-  const updateItem = (index: number, quantity: number) =>
-    enqueue(async () => {
-      UpdateItem({
+  const updateItem = (index: number, quantity: number) => {
+    lastUpdateToken.outdated = true
+    const token = (lastUpdateToken = { outdated: false })
+
+    const updatedList =
+      quantity === 0
+        ? [...itemList.slice(0, index), ...itemList.slice(index + 1)]
+        : adjust(index, item => ({ ...item, quantity }), itemList)
+
+    setItemList(updatedList)
+
+    const task = async () => {
+      const {
+        data: {
+          updateItems: { items },
+        },
+      } = await UpdateItem({
         variables: {
           orderItems: [
             {
@@ -37,22 +77,30 @@ const OrderItemsProvider = compose(
             },
           ],
         },
-        refetchQueries: [
-          {
-            query: ItemList.default,
-          },
-        ],
       })
+
+      if (!token.outdated) {
+        setItemList(items)
+      }
+    }
+
+    enqueue(task).catch((error: any) => {
+      if (!error || error.code !== 'TASK_CANCELLED') {
+        throw error
+      }
     })
+  }
 
   return (
-    <OrderItemsContext.Provider value={{ itemList, updateItem: updateItem }}>
+    <OrderItemsContext.Provider
+      value={{ itemList, updateItem: updateItem, loading: false }}
+    >
       {children}
     </OrderItemsContext.Provider>
   )
 })
 
-const useOrderItems = () => {
+export const useOrderItems = () => {
   const context = useContext(OrderItemsContext)
   if (context === undefined) {
     throw new Error('useOrderItems must be used within a OrderItemsProvider')
