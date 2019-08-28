e6e8cc865935b63981390fcaa37d15500f3c4d83
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-08-28 16:14:32 -0300

    Update tests

--- react/__mocks__/vtex.order-manager/OrderManager.tsx
+++ react/__mocks__/vtex.order-manager/OrderManager.tsx
@@ -1,4 +1,38 @@
-export const useOrderManager = () => ({
-  enqueue: (f: () => any) => f(),
-  listen: jest.fn(),
-})
+import React, { createContext, FC, useContext, useMemo, useState } from 'react'
+
+import { mockOrderForm } from '../mockOrderForm'
+
+interface Context {
+  enqueue: (_: any) => Promise<any>
+  listen: () => () => void
+  loading: boolean
+  orderForm: any
+  setOrderForm: (_: any) => void
+}
+
+const OrderManagerContext = createContext<Context | undefined>(undefined)
+
+export const OrderManagerProvider: FC = ({ children }: any) => {
+  const [orderForm, setOrderForm] = useState(mockOrderForm)
+
+  const value = useMemo(
+    () => ({
+      enqueue: async (f: any) => f(),
+      listen: () => () => {},
+      loading: false,
+      orderForm,
+      setOrderForm,
+    }),
+    [orderForm]
+  )
+
+  return (
+    <OrderManagerContext.Provider value={value}>
+      {children}
+    </OrderManagerContext.Provider>
+  )
+}
+
+export const useOrderManager = () => {
+  return useContext(OrderManagerContext)
+}
