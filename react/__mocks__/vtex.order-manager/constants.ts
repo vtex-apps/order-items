f6e835e758336ef5468999e5206963446031aadf
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-12-21 12:06:28 -0300

    Fix and tidy up tests

---
+++ react/__mocks__/vtex.order-manager/constants.ts
@@ -0,0 +1,1 @@
+export const TASK_CANCELLED_CODE = 'TASK_CANCELLED'
--- react/__mocks__/vtex.order-manager/index.ts
+++ react/__mocks__/vtex.order-manager/index.ts
@@ -1,4 +1,5 @@
 import * as OrderForm from './OrderForm'
 import * as OrderQueue from './OrderQueue'
+import * as constants from './constants'
 
-export { OrderForm, OrderQueue }
+export { OrderForm, OrderQueue, constants }
